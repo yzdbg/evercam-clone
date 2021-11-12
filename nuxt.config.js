337dcb6bd5e843edf679a2fdb19d69a5d3b7dba9
Yazid Bouguedra <yazid.bouguedra@evercam.io>
2021-11-12 16:37:48 +0100

    router changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/<repository-name>/'
+    base: '/evercam-clone/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
